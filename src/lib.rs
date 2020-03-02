40ea4576ede7d24ab865cd0499895b61d2ddb779
Martin Garton <garton@gmail.com>
2020-03-02 17:56:03 +0000

    Refactor to have a better API
    This is making progress.  The code is still a mess but it "works" and
    has a very simple happy-path integration test.

--- src/lib.rs
+++ src/lib.rs
@@ -5,112 +5,125 @@ pub mod proximo {
 use proximo::message_sink_client::MessageSinkClient;
 use proximo::{Message, PublisherRequest, StartPublishRequest};
 use tokio::sync::mpsc;
-use tonic::transport::Channel;
 use tonic::Request;
 
 pub struct Sink {
-    client: MessageSinkClient<Channel>,
-    topic: String,
+    //    client: MessageSinkClient<Channel>,
+    reqs: mpsc::Sender<MessageRequest>,
+}
+
+struct MessageRequest {
+    m: Message,
+    done: mpsc::Sender<Result<(), ProximoError>>,
 }
 
 impl Sink {
-    pub async fn new(
-        url: &str,
-        topic: &str,
-    ) -> Result<Sink, Box<dyn std::error::Error>> {
-        let client = MessageSinkClient::connect(url.to_string()).await?;
-
-        Ok(Sink {
-            client,
-            topic: topic.to_string(),
-        })
-    }
-
-    pub async fn publish_messages(
-        &mut self,
-        mut cancel: mpsc::Receiver<()>,
-        mut messages: mpsc::Receiver<Message>,
-        mut acks: mpsc::Sender<Message>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let topic = self.topic.to_string();
+    pub async fn new(url: &str, topic: &str) -> Result<Sink, ProximoError> {
+        let mut client = MessageSinkClient::connect(url.to_string()).await?;
 
         let (mut toack_tx, mut toack_rx) = mpsc::channel(16);
+        let (reqs_tx, mut reqs_rx): (
+            mpsc::Sender<MessageRequest>,
+            mpsc::Receiver<MessageRequest>,
+        ) = mpsc::channel(16);
+
+        let topic1 = topic.to_owned();
 
         let outbound = async_stream::stream! {
-            let req = PublisherRequest{
-                msg: None,
-                start_request: Some(StartPublishRequest{topic}),
-            };
-            yield req;
 
-            loop {
-                // TODO: Deal with cancellation, assuming we need to here?
+            yield PublisherRequest{msg: None, start_request: Some(StartPublishRequest{topic:topic1})};
 
-                match messages.recv().await {
+            loop { // TODO: Deal with cancellation, assuming we need to here?
+                match reqs_rx.recv().await {
                     None => {
                         panic!("empty message : when does this happen?")
                     }
-                    Some(msg) => {
-                        let req = PublisherRequest {
-                                msg: Some(msg.clone()),
+                    Some(req) => {
+                        let pubReq = PublisherRequest {
+                                msg: Some(req.m),
                                 start_request:None,
                         };
-
-                        match toack_tx.send(msg).await {
-                            Ok(()) => {
-                                // do nothing
-                            }
+                        match toack_tx.send(req.done).await {
+                            Ok(()) => {}
                             Err(e) => {
                                 panic!("handle this properly")
                             }
                         }
-
-                        yield req;
+                        yield pubReq;
                     }
                 }
             }
         };
 
-        let response = self.client.publish(Request::new(outbound)).await?;
+        tokio::spawn(async move {
+            let response = client.publish(Request::new(outbound)).await;
 
-        let mut inbound = response.into_inner();
-
-        loop {
-            tokio::select! {
-
-                c = cancel.recv() => {
-                    match c {
-                        None => panic!(
-                            "empty cancel.  when does this happen?"
-                        ),
-                        Some(()) => {
-                            // cancelled, so return
-                            println!("cancel recv'd - exiting");
-                            return Ok(());
-                        }
-                    }
+            let response = match response {
+                Ok(resp) => resp,
+                Err(_e) => {
+                    panic!("handle this properly");
                 }
+            };
 
-                m = inbound.message() => {
-                    match m? {
-                        None => panic!(
-                            "empty message from proximo.  when does this happen?"
-                        ),
-                        Some(conf) => match toack_rx.recv().await {
-                            None => panic!("when does this happen?"),
-                            Some(to_ack) => {
-                                if to_ack.id != conf.msg_id {
-                                    return Err(Box::new(ProximoError::new(
-                                        "unexpected ack order",
-                                    )));
-                                }
-                                acks.send(to_ack).await?;
-                            }
-                        },
+            let mut inbound = response.into_inner();
 
+            loop {
+                let m = inbound.message().await;
+                let m1;
+                match m {
+                    Ok(m) => m1 = m,
+                    Err(_e) => {
+                        panic!("error getting grpc stream response. deal with this properly")
                     }
+                };
+
+                match m1 {
+                    None => panic!(
+                        "empty message from proximo.  when does this happen?"
+                    ),
+                    Some(_conf) => match toack_rx.recv().await {
+                        None => panic!("when does this happen?"),
+                        Some(mut to_ack) => {
+                            /*
+                            TODO: check the id is the one expected?
+                            if to_ack.id != conf.msg_id {
+                                return Err(Box::new(ProximoError::new(
+                                    "unexpected ack order",
+                                )));
+                            }
+                            */
+                            match to_ack.send(Ok(())).await {
+                                Ok(()) => {}
+                                Err(_e) => panic!("handle properly"),
+                            }
+                        }
+                    },
                 }
             }
+        });
+
+        Ok(Sink {
+            // client,
+            reqs: reqs_tx,
+        })
+    }
+
+    pub async fn send_message(
+        &mut self,
+        m: Message,
+    ) -> Result<(), ProximoError> {
+        let (done_tx, mut done_rx) =
+            mpsc::channel::<Result<(), ProximoError>>(16);
+
+        let req = MessageRequest { m, done: done_tx };
+
+        self.reqs.send(req).await?;
+
+        match done_rx.recv().await {
+            None => {
+                panic!("no recv. what?");
+            }
+            Some(recv_res) => Ok(recv_res?),
         }
     }
 }
@@ -118,20 +131,38 @@ impl Sink {
 use std::fmt;
 
 #[derive(Debug)]
-struct ProximoError {
+pub struct ProximoError {
     err: String,
 }
 
 impl std::error::Error for ProximoError {}
 
-impl ProximoError {
-    fn new(s: &str) -> Self {
-        ProximoError { err: s.to_string() }
-    }
-}
-
 impl fmt::Display for ProximoError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Oh no, something bad went down")
+    }
+}
+
+impl From<tonic::Status> for ProximoError {
+    fn from(err: tonic::Status) -> ProximoError {
+        ProximoError {
+            err: format!("{}", err),
+        }
+    }
+}
+
+impl From<tonic::transport::Error> for ProximoError {
+    fn from(err: tonic::transport::Error) -> ProximoError {
+        ProximoError {
+            err: format!("{}", err),
+        }
+    }
+}
+
+impl From<mpsc::error::SendError<MessageRequest>> for ProximoError {
+    fn from(err: mpsc::error::SendError<MessageRequest>) -> ProximoError {
+        ProximoError {
+            err: format!("{}", err),
+        }
     }
 }
---
+++ tests/integration_test.rs
@@ -0,0 +1,92 @@
+extern crate proximo_client;
+
+use proximo_client::proximo::Message;
+use proximo_client::Sink;
+use std::io;
+use std::process::{Child, Command, Stdio};
+use std::thread;
+use std::time;
+
+struct ChildDropper {
+    children: Vec<Child>,
+}
+
+#[tokio::test]
+async fn test_all() {
+    do_test_all().await.unwrap();
+}
+
+async fn do_test_all() -> io::Result<()> {
+    let mut cmds = ChildDropper::new();
+
+    let nats = Command::new("nats-streaming-server")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()?;
+
+    cmds.track(nats);
+
+    let proximo = Command::new("proximo-server")
+        .args(&["nats-streaming"])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()?;
+
+    cmds.track(proximo);
+
+    thread::sleep(time::Duration::from_millis(1000));
+
+    let plumber = Command::new("plumber")
+        .args(&[
+            "consume-all-raw",
+            "--sourceurl",
+            "nats-streaming://localhost:4222/topic1?cluster-id=test-cluster&queue-group=1",
+        ])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::null())
+        .spawn()?;
+
+    cmds.track(plumber);
+
+    do_publishes().await.unwrap();
+
+    Ok(())
+}
+
+async fn do_publishes() -> Result<(), Box<dyn std::error::Error>> {
+    let mut s = Sink::new("http://localhost:6868", "topic1").await?;
+
+    for id in 1..10 {
+        let id = id.to_string();
+
+        let data = format!("Hello from message id {}", id).into_bytes();
+
+        let m = Message { id, data };
+
+        s.send_message(m).await?;
+    }
+
+    Ok(())
+}
+
+/// ChildDropper tracks child processes and kills them when dropped.
+impl ChildDropper {
+    fn new() -> Self {
+        ChildDropper {
+            children: Vec::new(),
+        }
+    }
+
+    fn track(&mut self, c: Child) {
+        self.children.push(c)
+    }
+}
+
+impl std::ops::Drop for ChildDropper {
+    fn drop(&mut self) {
+        for child in self.children.iter_mut() {
+            child.kill().unwrap();
+        }
+    }
+}
+
